df19ce58f4b09797edebcbddaddee2701d276845
root <root@localhost.localdomain>
2018-06-26 13:37:00 -0300

    index finish

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include "header.php";
+?>
+<div class="container-fluid space2"></div>
+<div class="container-fluid container-profile-pic">
+	<img class="profile-pic" src="assets/profile.jpg">
+</div>
+<div class="container-fluid space"></div>
+<div class="container-fluid container-name">
+	Bruno Vilela
+</div>
+<div class="container-fluid container-desc">
+	Undergraduate | Developer | Kaggle Novice
+</div>
+<div class="container-fluid space"></div>
+
+<div class="container-fluid container-about-me">
+	<div class="container">
+		I am a software developer with experience in web and android development. I am also a novice Kaggler who is interested mainly about natural language processing in artificial intelligence area. As I learn everyday, my main programming languages are Python, Java and PHP, I do use Linux most of the time and with some basic knowledge of Machine Learning, Linear Programming, Deep Learning, ...
+	</div>
+</div>
+
+
+<?php
+include "footer.php";
+?>
---
+++ portfolio.php
@@ -0,0 +1,10 @@
+<?php
+include "header.php";
+?>
+
+<div class="container-fluid container-name">
+	Portfolio
+</div>
+<?php
+include "footer.php";
+?>
