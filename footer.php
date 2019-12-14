8622ef0f862e78881f7cd1a273ae7aef1b5b3433
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2019-12-14 18:23:59 -0300

    added profile img

--- footer.php
+++
@@ -1,9 +0,0 @@
-<div class="space2"></div>
-<div class="container-fluid footer">
-	<a href="https://www.facebook.com/bruno.vilela00/" target="_blank" class="fa fa-facebook"></a>
-	<a href="https://web.whatsapp.com/send?phone=5535998217355" target="_blank" class="fa fa-whatsapp footer-item"></a>
-	<a href="https://www.linkedin.com/in/vilelabruno00/" target="_blank" class="fa fa-linkedin footer-item"></a>
-	<a href="https://github.com/vilelabruno" target="_blank" class="fa fa-github footer-item"></a>
-</div>
-<div class="space2"></div>
-</body></html>
